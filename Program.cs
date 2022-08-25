5123141562791319f2e7cc4144d9228f180ed06d
Madina <madina.orish@mail.ru>
2022-08-25 18:42:57 +0300

    1 задача

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+ /*   
+// Ввод данных
+
+        int num1;
+        int num2;
+
+        Console.Write("Введите первое число: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        int max = num1;
+// Условия вывода данных
+        if (num2 > max) max = num2;
+    
+
+// Вывод даных
+        Console.Write("Максимальное число: " + max);
+    
+*/
